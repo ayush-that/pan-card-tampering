4392955b106aebb3c58897c3b7c605008ec5c5ba
ayush-that <ayush1337@hotmail.com>
2024-08-30 09:34:52 +0530

    chore: Update wsgi.py to use app.run() for running the application

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,4 @@
 from app import app
 
-if __name__ != "__main__":
-    application = app
-
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    app.run()
